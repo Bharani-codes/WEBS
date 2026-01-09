a8683294b5f509eb6ff3999f080b97d427a48035
BHARANI DHARAN K <bharanidharancce@gmail.com>
2026-01-09 11:38:34 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function start() {
+  const email = document.querySelector("input").value;
+
+  if (email === "") {
+    alert("Enter your email address");
+  } else {
+    alert("Welcome! This is a demo clone.");
+  }
+}
